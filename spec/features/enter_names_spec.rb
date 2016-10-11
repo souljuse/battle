e0e31990da63e28b9ad94b20f68da5044aaf1ea5
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-11 16:50:14 +0100

    Failing test for name entry

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+
+feature 'Enter names' do
+  it 'allows players to enter their names' do
+    visit('/')
+    fill_in :player_1_name, with: "Bob"
+    fill_in :player_2_name, with: "Larry"
+    click_button 'Submit'
+    expect(page).to have_text ("Bob")
+  end
+end
