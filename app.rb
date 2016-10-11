1ae36d69b080bf8867058218c6ab3e8bb38892af
Amaal Ali <amq.ali@gmail.com>
2016-10-11 17:39:11 +0100

    Refactored using POST-redirect loop

--- app.rb
+++ app.rb
@@ -1,13 +1,28 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  # enable :sessions
+  use Rack::Session::Cookie,:key => 'rack.session',
+                            :path => '/',
+                            :secret => 'your_secret'
+
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    # session['names'] = params['names']
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    p @player_1_name 
+    p session[:player_1_name]
     erb :play
   end
 
