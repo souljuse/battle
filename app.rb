72df339b9f0b366799a31d91e9a5afd2ba74c738
iaink <iainkinnish@googlemail.com>
2016-10-12 15:36:20 +0000

    Completed Chapter 24.

--- app.rb
+++ app.rb
@@ -2,33 +2,29 @@ require 'sinatra/base'
 require './lib/player'
 
 class Battle < Sinatra::Base
-  
-  $player_1 = Player.new(@player_1_name)
-  $player_2 = Player.new(@player_2_name)
 
-  enable :sessions
+ get '/' do
+   erb :index
+ end
 
-  get '/' do
-    erb :index
-  end
+ post '/names' do
+   $player_1 = Player.new(params[:player_1_name])
+   $player_2 = Player.new(params[:player_2_name])
+   redirect '/play'
+ end
 
-  post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
-    redirect '/play'
-  end
+ get '/play' do
+   @player_1_name = $player_1.name
+   @player_2_name = $player_2.name
+   erb(:play)
+ end
 
-  get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
-    erb(:play)
-  end
-
-  get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
-    erb(:attack)
-  end
-  # start the server if ruby file executed directly
-  run! if app_file == $0
+ get '/attack' do
+   @player_1 = $player_1
+   @player_2 = $player_2
+   @player_1.attack(@player_2)
+   erb(:attack)
+ end
+ # start the server if ruby file executed directly
+ run! if app_file == $0
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,19 @@
 class Player
+
+  attr_reader :name, :hit_points
+
+  DEFAULT_HITPOINTS = 50
   
-  attr_reader :name
-  
-  def initialize(name)
+  def initialize(name, hit_points = DEFAULT_HITPOINTS)
     @name = name
+    @hit_points = hit_points
   end
   
-  def attack
-    
+  def attack(player)
+    player.receive_damage
+  end
+  
+  def receive_damage
+    @hit_points -= 10
   end
 end
