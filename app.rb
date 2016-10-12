c72e5de4e666c4374c7ab51ffa4aeff64e2bb8cf
crispinandrews <crispin.andrews@gmail.com>
2016-10-12 16:46:18 +0100

    Avoid merge conflict

--- app.rb
+++ app.rb
@@ -3,16 +3,14 @@ require './lib/player'
 
 class Battle < Sinatra::Base
 
-<<<<<<< HEAD
   # $player_1 = Player.new(@player_1_name)
   # $player_2 = Player.new(@player_2_name)
 
   # enable :sessions
-=======
+
  get '/' do
    erb :index
  end
->>>>>>> 72df339b9f0b366799a31d91e9a5afd2ba74c738
 
  post '/names' do
    $player_1 = Player.new(params[:player_1_name])
@@ -20,7 +18,6 @@ class Battle < Sinatra::Base
    redirect '/play'
  end
 
-<<<<<<< HEAD
   post '/names' do
     $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
@@ -41,7 +38,7 @@ class Battle < Sinatra::Base
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
-=======
+
  get '/play' do
    @player_1_name = $player_1.name
    @player_2_name = $player_2.name
