508fc36a80470abe2df6258f7d95dbaa59905d66
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-12 13:29:02 +0200

    completed challenge 20 and added a session to make it work with shotgun and added Player 2 hit points

--- app.rb
+++ app.rb
@@ -1,18 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  use Rack::Session::Cookie,  :key => 'rack.session',
+                              :path => '/',
+                              :secret => 'your_secret'
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:Player1]
-    @player_2_name = params[:Player2]
-    erb(:play)
+    session[:player_1_name] = params[:Player1]
+    session[:player_2_name] = params[:Player2]
+    redirect '/play'
   end
 
   get '/play' do
-
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -7,3 +7,14 @@ feature 'enter names' do
 		expect(page).to have_content('Tom versus Jerry')
 	end
 end
+
+
+feature 'show points' do
+	scenario 'will show player 2s hit points to player 1' do
+		visit('/')
+		fill_in(:Player1, with: 'Tom')
+		fill_in(:Player2, with: 'Jerry')
+		click_button('Submit')
+		expect(page).to have_content('Jerry has 100 hit points')
+	end
+end
