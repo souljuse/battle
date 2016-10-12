2ac70dfc9e31f5c0389a4d1da30db157a4974b4f
crispinandrews <crispin.andrews@gmail.com>
2016-10-12 14:41:32 +0100

    Stage 23. Global variables added

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
 feature 'Enter names' do
   it 'allows players to enter their names' do
     sign_in_and_play
-    expect(page).to have_content ("Bob")
+    expect(page).to have_content ("Larry")
   end
 
   it 'shows the players HP' do
     sign_in_and_play
-    expect(page).to have_content ("Bob :30HP")
+    expect(page).to have_content ("Larry :50HP")
   end
 end
