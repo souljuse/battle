187a8fab3885ce65331cc1524387c06a6f3fe64e
souljuse <oppo.irene@gmail.com>
2016-10-13 14:50:21 +0200

    step 28 completed

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -9,7 +9,13 @@ feature "Attack player 2" do
 
   scenario "it should reduce Player 2's HP by 10" do
     sign_in_and_play
-    click_button("Attack!")
+    attack
     expect(page).to have_content("Jerry has 90 hit points")
+  end
+  scenario "it should reduce Player 1's HP by 10" do
+    sign_in_and_play
+    attack
+    attack
+    expect(page).to have_content("Tom has 90 hit points")
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 feature 'show points' do
-	scenario 'will show player 2s hit points to player 1' do
+	scenario 'will show player 2s hit points' do
 		sign_in_and_play
 		expect(page).to have_content('Jerry has 100 hit points')
+	end
+end
+
+feature 'show points' do
+	scenario 'will show player 1s hit points' do
+		sign_in_and_play
+		expect(page).to have_content('Tom has 100 hit points')
 	end
 end
